b83a4264a464785406942b10d2600914303bc1a0
bezos <151596652+Bezayaecob@users.noreply.github.com>
2026-02-13 16:21:36 +0300

    Add sumEvenAfterQueries method to calculate even sums
    Implement sumEvenAfterQueries method to calculate the sum of even numbers after each query.

---
+++ sum-of-even-numbers-after-queries.py
@@ -0,0 +1,24 @@
+
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        result = []
+        sum_even = sum(num for num in nums if num % 2 == 0)  
+
+        for value, index in queries:
+            
+            if nums[index] % 2 == 0:
+                sum_even -= nums[index]  
+            
+            
+            nums[index] += value
+            
+           
+            if nums[index] % 2 == 0:
+                sum_even += nums[index]  
+            
+            
+            result.append(sum_even)
+
+        return result
+
+        
