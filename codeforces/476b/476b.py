e1f8243a49b4d2e9c6b2bafa965c909ed71a8ed7
bezos <151596652+Bezayaecob@users.noreply.github.com>
2026-04-15 13:58:16 +0300

    Add solution for 476b

---
+++ codeforces/476b/476b.py
@@ -0,0 +1,24 @@
+s1 = input()
+s2= input()
+for i in range(len(s1)):
+    if s1[i] == '+':
+        val += 1
+    else:
+        val -= 1
+
+res = []
+def recurse(curr, n):
+    if n == len(s1):
+        res.append(curr)
+        curr = 0
+        return
+    if s2[n] == '+':
+        recurse(curr+1, n+1)
+    elif s2[n] == '-':
+        recurse(curr-1, n+1)
+    else:
+        recurse(curr-1, n+1)
+        recurse(curr+1, n+1)
+recurse(0, 0)
+ans = res.count(val)
+print((ans/len(res)))
