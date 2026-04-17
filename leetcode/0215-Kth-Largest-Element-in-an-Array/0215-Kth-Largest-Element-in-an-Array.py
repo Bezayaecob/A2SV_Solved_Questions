d416378c3d543abd668fb6e508672f474a9e7baa
bezos <151596652+Bezayaecob@users.noreply.github.com>
2026-04-17 15:35:07 +0300

    Add solution for Kth Largest Element in an Array

---
+++ leetcode/0215-Kth-Largest-Element-in-an-Array/0215-Kth-Largest-Element-in-an-Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+       
+            low = min(nums)
+            high= max(nums)
+            
+            while low <= high:
+                mid = (low + high) // 2
+                count=0
+                for num in nums:
+                    if num>= mid:
+                        count+=1
+
+
+                if count >= k:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+            return high
